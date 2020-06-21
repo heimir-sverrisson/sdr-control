4d0ab48cf3322c832c179ce5838b5eee55eb7744
Heimir Thor Sverrisson <heimir.sverrisson@gmail.com>
2020-06-21 13:07:40 -0600

    Changing address and default port to 4242

--- receive_frequency.py
+++ receive_frequency.py
@@ -1,13 +1,14 @@
 import socket
 import json
 
-UDP_IP = "192.168.42.88"
-UDP_PORT = 4200
+UDP_IP = "192.168.86.213"
+UDP_PORT = 4242
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
 while True:
     data, addr = sock.recvfrom(1024)
+    print(data)
     setting = json.loads(data)
-    print(setting["uplink"])
+    print(setting["mode"])
